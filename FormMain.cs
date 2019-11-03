227c03ca0964677ae74185280b0746afb62d1bf1
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2019-11-03 19:09:37 +0900

    プロジェクト ファイルを追加します。

---
+++ FormMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GrobalHook;
+
+namespace GrobalHookMouseWinFormCSharp
+{
+    public partial class FormMain : Form
+    {
+        private static IntPtr m_hHook = IntPtr.Zero;
+        MouseHook.HookProcedureDelegate m_proc;
+        private const int WH_MOUSE_LL = 14;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            if (m_hHook == IntPtr.Zero)
+            {
+                m_proc = new MouseHook.HookProcedureDelegate(MouseHookProc);
+                SetMouseHook(m_proc);
+            }
+            else
+            {
+                RemoveMouseHook();
+            }
+        }
+
+        private bool SetMouseHook(MouseHook.HookProcedureDelegate _proc)
+        {
+            bool bResult = true;
+
+            using (Process process = Process.GetCurrentProcess())
+            using (ProcessModule processModule = process.MainModule)
+            {
+                m_hHook = MouseHook.SetWindowsHookEx(WH_MOUSE_LL, _proc, MouseHook.GetModuleHandle(processModule.ModuleName), 0);
+                if (m_hHook == IntPtr.Zero)
+                {
+                    {
+                        MessageBox.Show("SetWindowsHookEx Failed.");
+                    }
+                }
+            }
+
+            return bResult;
+        }
+
+        private void RemoveMouseHook()
+        {
+            if (MouseHook.UnhookWindowsHookEx(m_hHook) == false)
+            {
+                MessageBox.Show("UnhookWindowsHookEx Failed.");
+            }
+        }
+
+        public IntPtr MouseHookProc(int _nCode, IntPtr _wParam, IntPtr _lParam)
+        {
+            if (_nCode >= 0)
+            {
+                MouseHook.MouseHookStruct mouseHookStruct = (MouseHook.MouseHookStruct)Marshal.PtrToStructure(_lParam, typeof(MouseHook.MouseHookStruct));
+
+                String strText = "x = " + mouseHookStruct.pt.x.ToString("d") + " : y = " + mouseHookStruct.pt.y.ToString("d");
+                this.Text = strText;
+            }
+            return MouseHook.CallNextHookEx(m_hHook, _nCode, _wParam, _lParam);
+        }
+    }
+}
---
+++ GrobalHook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrobalHook
+{
+    public static class MouseHook
+    {
+        public delegate IntPtr HookProcedureDelegate(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HookProcedureDelegate lpfn, IntPtr hInstance, int threadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(IntPtr idHook);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(String lpModuleName);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class MouseHookStruct
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+    }
+}
